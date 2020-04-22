66c28c47df449cab87ebf639f6481df94f2b5d12
Olubiyi Emmanuel <42422184+coded1guy@users.noreply.github.com>
2020-04-22 07:35:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ function calcLoan() {
 	var rate = Number(document.getElementById('rate').value);
 	var time = Number(document.getElementById('time').value);
 	var result = (amount*rate*time) / 100;
-	document.getElementById("loan_result").innerHTML = "You are to pay " + "NGN " + result.toFixed(2) + " by the end of " + time + " years.";
+	document.getElementById("loan_result").innerHTML = "You are to pay " + "NGN " + amount+result.toFixed(2) + " by the end of " + time + " years.";
 }
